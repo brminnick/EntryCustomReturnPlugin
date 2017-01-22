ce13bf780e0d01458b587c5379c3d69364656695
brminnick <brandon.r.minnick@gmail.com>
2017-01-22 15:49:49 -0800

    Added OnElementPropertyChanged to CustomRenderers

--- Src/EntryCustomReturn.Forms.Plugin.Android/CustomReturnEntryRenderer.cs
+++ Src/EntryCustomReturn.Forms.Plugin.Android/CustomReturnEntryRenderer.cs
@@ -14,66 +14,80 @@ using EntryCustomReturn.Forms.Plugin.Abstractions;
 [assembly: ExportRenderer(typeof(CustomReturnEntry), typeof(CustomReturnEntryRenderer))]
 namespace EntryCustomReturn.Forms.Plugin.Droid
 {
-    /// <summary>
-    /// CustomReturnEntry Implementation
-    /// </summary>
-    [Preserve(AllMembers = true)]
-    public class CustomReturnEntryRenderer : EntryRenderer
-    {
-        /// <summary>
-        /// Used for registration with dependency service
-        /// </summary>
-        public new async static Task Init()
-        {
-            var temp = DateTime.Now;
-        }
+	/// <summary>
+	/// CustomReturnEntry Implementation
+	/// </summary>
+	[Preserve(AllMembers = true)]
+	public class CustomReturnEntryRenderer : EntryRenderer
+	{
+		/// <summary>
+		/// Used for registration with dependency service
+		/// </summary>
+		public new async static Task Init()
+		{
+			var temp = DateTime.Now;
+		}
 
-        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
-        {
-            base.OnElementChanged(e);
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
 
-            var customEntry = Element as Abstractions.CustomReturnEntry;
+			var customEntry = Element as Abstractions.CustomReturnEntry;
 
-            if (Control != null && customEntry != null)
-            {
-                SetKeyboardButtonType(customEntry.ReturnType);
+			if (Control != null && customEntry != null)
+			{
+				SetKeyboardButtonType(customEntry.ReturnType);
 
-                Control.EditorAction += (object sender, TextView.EditorActionEventArgs args) =>
-                {
-                    if (customEntry?.ReturnType != ReturnType.Next)
-                        customEntry?.Unfocus();
+				Control.EditorAction += (object sender, TextView.EditorActionEventArgs args) =>
+				{
+					if (customEntry?.ReturnType != ReturnType.Next)
+						customEntry?.Unfocus();
 
-                    customEntry?.InvokeCompleted();
-                };
-            }
-        }
+					customEntry?.InvokeCompleted();
+				};
+			}
+		}
 
-        void SetKeyboardButtonType(ReturnType returnType)
-        {
-            switch (returnType)
-            {
-                case ReturnType.Go:
-                    Control.ImeOptions = ImeAction.Go;
-                    Control.SetImeActionLabel("Go", ImeAction.Go);
-                    break;
-                case ReturnType.Next:
-                    Control.ImeOptions = ImeAction.Next;
-                    Control.SetImeActionLabel("Next", ImeAction.Next);
-                    break;
-                case ReturnType.Send:
-                    Control.ImeOptions = ImeAction.Send;
-                    Control.SetImeActionLabel("Send", ImeAction.Send);
-                    break;
-                case ReturnType.Search:
-                    Control.ImeOptions = ImeAction.Search;
-                    Control.SetImeActionLabel("Search", ImeAction.Search);
-                    break;
-                default:
-                    Control.ImeOptions = ImeAction.Done;
-                    Control.SetImeActionLabel("Done", ImeAction.Done);
-                    break;
-            }
-        }
-    }
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (e.PropertyName == CustomReturnEntry.ReturnTypeProperty.PropertyName)
+			{
+				var customEntry = sender as CustomReturnEntry;
+
+				if (Control != null && customEntry != null)
+					SetKeyboardButtonType(customEntry.ReturnType);
+			}
+
+		}
+
+		void SetKeyboardButtonType(ReturnType returnType)
+		{
+			switch (returnType)
+			{
+				case ReturnType.Go:
+					Control.ImeOptions = ImeAction.Go;
+					Control.SetImeActionLabel("Go", ImeAction.Go);
+					break;
+				case ReturnType.Next:
+					Control.ImeOptions = ImeAction.Next;
+					Control.SetImeActionLabel("Next", ImeAction.Next);
+					break;
+				case ReturnType.Send:
+					Control.ImeOptions = ImeAction.Send;
+					Control.SetImeActionLabel("Send", ImeAction.Send);
+					break;
+				case ReturnType.Search:
+					Control.ImeOptions = ImeAction.Search;
+					Control.SetImeActionLabel("Search", ImeAction.Search);
+					break;
+				default:
+					Control.ImeOptions = ImeAction.Done;
+					Control.SetImeActionLabel("Done", ImeAction.Done);
+					break;
+			}
+		}
+	}
 }
 
--- Src/EntryCustomReturn.Forms.Plugin.iOSUnified/CustomReturnEntryRenderer.cs
+++ Src/EntryCustomReturn.Forms.Plugin.iOSUnified/CustomReturnEntryRenderer.cs
@@ -13,59 +13,73 @@ using EntryCustomReturn.Forms.Plugin.Abstractions;
 [assembly: ExportRenderer(typeof(CustomReturnEntry), typeof(CustomReturnEntryRenderer))]
 namespace EntryCustomReturn.Forms.Plugin.iOS
 {
-    [Preserve(AllMembers = true)]
-    public class CustomReturnEntryRenderer : EntryRenderer
-    {
-        /// <summary>
-        /// Used for registration with the dependency service
-        /// </summary>
-        /// <returns></returns>
-        public new async static Task Init()
-        {
-            var temp = DateTime.Now;
-        }
+	[Preserve(AllMembers = true)]
+	public class CustomReturnEntryRenderer : EntryRenderer
+	{
+		/// <summary>
+		/// Used for registration with the dependency service
+		/// </summary>
+		/// <returns></returns>
+		public new async static Task Init()
+		{
+			var temp = DateTime.Now;
+		}
 
-        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
-        {
-            base.OnElementChanged(e);
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
 
-            var customEntry = Element as Abstractions.CustomReturnEntry;
+			var customEntry = Element as CustomReturnEntry;
 
-            if (Control != null && customEntry != null)
-            {
-                SetKeyboardButtonType(customEntry.ReturnType);
+			if (Control != null && customEntry != null)
+			{
+				SetKeyboardButtonType(customEntry.ReturnType);
 
-                Control.ShouldReturn += (UITextField tf) =>
-                {
-                    customEntry?.InvokeCompleted();
-                    return true;
-                };
-            }
-        }
+				Control.ShouldReturn += (UITextField tf) =>
+				{
+					customEntry?.InvokeCompleted();
+					return true;
+				};
+			}
+		}
 
-        void SetKeyboardButtonType(ReturnType returnType)
-        {
-            switch (returnType)
-            {
-                case ReturnType.Go:
-                    Control.ReturnKeyType = UIReturnKeyType.Go;
-                    break;
-                case ReturnType.Next:
-                    Control.ReturnKeyType = UIReturnKeyType.Next;
-                    break;
-                case ReturnType.Send:
-                    Control.ReturnKeyType = UIReturnKeyType.Send;
-                    break;
-                case ReturnType.Search:
-                    Control.ReturnKeyType = UIReturnKeyType.Search;
-                    break;
-                case ReturnType.Done:
-                    Control.ReturnKeyType = UIReturnKeyType.Done;
-                    break;
-                default:
-                    Control.ReturnKeyType = UIReturnKeyType.Default;
-                    break;
-            }
-        }
-    }
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (e.PropertyName == CustomReturnEntry.ReturnTypeProperty.PropertyName)
+			{
+				var customEntry = sender as CustomReturnEntry;
+
+				if (Control != null && customEntry != null)
+					SetKeyboardButtonType(customEntry.ReturnType);
+			}
+
+		}
+
+		void SetKeyboardButtonType(ReturnType returnType)
+		{
+			switch (returnType)
+			{
+				case ReturnType.Go:
+					Control.ReturnKeyType = UIReturnKeyType.Go;
+					break;
+				case ReturnType.Next:
+					Control.ReturnKeyType = UIReturnKeyType.Next;
+					break;
+				case ReturnType.Send:
+					Control.ReturnKeyType = UIReturnKeyType.Send;
+					break;
+				case ReturnType.Search:
+					Control.ReturnKeyType = UIReturnKeyType.Search;
+					break;
+				case ReturnType.Done:
+					Control.ReturnKeyType = UIReturnKeyType.Done;
+					break;
+				default:
+					Control.ReturnKeyType = UIReturnKeyType.Default;
+					break;
+			}
+		}
+	}
 }

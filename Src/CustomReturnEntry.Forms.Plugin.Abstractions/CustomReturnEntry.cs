df6572b5b6d995e697fcd33a6b92087a5fcbcfc5
Brandon Minnick <Brandon.r.minnick@gmail.com>
2017-01-22 11:48:19 -0800

    Added CustomReturnEntry.Plugin.Abstractions

---
+++ Src/CustomReturnEntry.Forms.Plugin.Abstractions/CustomReturnEntry.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CustomReturnEntry.Forms.Plugin.Abstractions
+{
+    /// <summary>
+    /// CustomReturnEntry Interface
+    /// </summary>
+    public class CustomReturnEntry : Entry
+	{
+        /// <summary>
+        /// Occurs when the user finalizes the text in an entry with the return key
+        /// </summary>
+        public new event EventHandler Completed;
+
+        /// <summary>
+        /// Return Type Property of the Entry
+        /// </summary>
+        public static readonly BindableProperty ReturnTypeProperty =
+            BindableProperty.Create(propertyName: nameof(ReturnType),
+                returnType: typeof(ReturnType),
+                declaringType: typeof(CustomReturnEntry),
+                defaultValue: ReturnType.Done);
+
+        /// <summary>
+        /// Type of the Keyboard Return Key
+        /// </summary>
+        public ReturnType ReturnType
+        {
+            get { return (ReturnType)GetValue(ReturnTypeProperty); }
+            set { SetValue(ReturnTypeProperty, value); }
+        }
+
+        /// <summary>
+        /// Invoke Completed event
+        /// </summary>
+        internal void InvokeCompleted()
+        {
+            Completed?.Invoke(this, null);
+        }
+    }
+
+    /// <summary>
+    /// Custom type for keyboard return key
+    /// </summary>
+    public enum ReturnType
+    {
+        Go,
+        Next,
+        Done,
+        Send,
+        Search
+    }
+}

024787a497e1711760a8c9ecce0df2374d0a5398
brminnick <brandon.r.minnick@gmail.com>
2017-01-24 14:39:04 -0800

    Added Keyboard Helpers Static Class

---
+++ Src/EntryCustomReturn.Forms.Plugin.Android/KeyboardHelpers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+using Android.Widget;
+using Android.Views.InputMethods;
+
+using EntryCustomReturn.Forms.Plugin.Abstractions;
+
+namespace EntryCustomReturn.Forms.Plugin.Android
+{
+	static class KeyboardHelpers
+	{
+		internal static void SetKeyboardButtonType(ReturnType returnType, EditText control)
+		{
+			switch (returnType)
+			{
+				case ReturnType.Go:
+					control.ImeOptions = ImeAction.Go;
+					control.SetImeActionLabel("Go", ImeAction.Go);
+					break;
+				case ReturnType.Next:
+					control.ImeOptions = ImeAction.Next;
+					control.SetImeActionLabel("Next", ImeAction.Next);
+					break;
+				case ReturnType.Send:
+					control.ImeOptions = ImeAction.Send;
+					control.SetImeActionLabel("Send", ImeAction.Send);
+					break;
+				case ReturnType.Search:
+					control.ImeOptions = ImeAction.Search;
+					control.SetImeActionLabel("Search", ImeAction.Search);
+					break;
+				default:
+					control.ImeOptions = ImeAction.Done;
+					control.SetImeActionLabel("Done", ImeAction.Done);
+					break;
+			}
+		}
+	}
+}

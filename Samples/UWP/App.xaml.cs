0760b65d5aa1ada5961124cd8ca8bb32edc1f5af
Brandon Minnick <Brandon.r.minnick@gmail.com>
2017-03-18 20:05:17 -0700

    Use Global Identifier for Xamarin.Forms.Forms.Init

--- Samples/UWP/App.xaml.cs
+++ Samples/UWP/App.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Xamarin;
-using Xamarin.Forms;
 using Application = Windows.UI.Xaml.Application;
 using Frame = Windows.UI.Xaml.Controls.Frame;
 
@@ -60,7 +48,7 @@ namespace EntryCustomReturnSampleApp.UWP
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
-                Forms.Init(e);
+                global::Xamarin.Forms.Forms.Init(e);
                 EntryCustomReturn.Forms.Plugin.UWP.CustomReturnEntryRenderer.Init();
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)

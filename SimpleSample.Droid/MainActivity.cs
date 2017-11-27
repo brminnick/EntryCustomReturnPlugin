ed2f1b2c9c3e279fc2d88175644855a4b095beea
brminnick <brandon.r.minnick@gmail.com>
2017-11-26 16:50:28 -0800

    Updated SimpleSample.Droid

--- SimpleSample.Droid/MainActivity.cs
+++ SimpleSample.Droid/MainActivity.cs
@@ -2,19 +2,22 @@
 using Android.App;
 using Android.Content.PM;
 
+using EntryCustomReturn.Forms.Plugin.Android;
+
 namespace SimpleSample.Droid
 {
     [Activity(Label = "SimpleSample.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            CustomReturnEntryRenderer.Init();
 
             LoadApplication(new App());
         }

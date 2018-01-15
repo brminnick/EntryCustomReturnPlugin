231bb9a10ab4c419d1ccf4da7f9527478735f92b
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-01-15 12:46:06 -0800

    Updated Nuspec

--- EntryCustomReturn.Forms.Plugin.Abstractions_portable/Effects/EffectConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EntryCustomReturn.Forms.Plugin.Abstractions
-{
-    public static class EffectConstants
-	{
-        public const string ResolutionGroupName = "EntryCustomReturn.Forms.Plugin";
-		internal static string Name => $"{ResolutionGroupName}.{EffectName}";
-		const string EffectName = "CustomKeyboardReturnEffect";
-	}
-}
